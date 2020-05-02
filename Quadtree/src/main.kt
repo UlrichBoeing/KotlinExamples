2f436c777d38ea70680afdb1848e6d522f26f164
Ulrich <songs.of.letting.go@gmail.com>
2020-05-02 21:38:41 +0200

    more on Quadtree.kt

--- Quadtree/src/main.kt
+++ Quadtree/src/main.kt
@@ -1,9 +1,11 @@
 import de.ulrich_boeing.basics.*
 import de.ulrich_boeing.framework.drawAsCircles
-import de.ulrich_boeing.quadtree.IndexQuadtree
+import de.ulrich_boeing.processing.fillOnly
+import de.ulrich_boeing.processing.strokeOnly
 import de.ulrich_boeing.quadtree.Quadtree
 import processing.core.PApplet
-import kotlin.random.Random
+import de.ulrich_boeing.utility.MouseTrace
+import de.ulrich_boeing.utility.Timespan
 
 class MyApplet : PApplet() {
     companion object {
@@ -14,74 +16,62 @@ class MyApplet : PApplet() {
         }
     }
 
-    val rect by lazy { Rect(0, 0, width, height) }
-//    val quadtree by lazy { IndexQuadtree(rect, 4) }
-    var lastPoint = Vec(-1000, -1000)
-    var sum = 0L
+    val appWindow by lazy { Rect(width, height) }
+
+    val mouseTrace = MouseTrace(this)
+    var buildUp = Timespan("buildUp")
+    var query = Timespan("query")
+
+    val numParticles = 1000000
+    val numQueries = 10000
+
 
     override fun setup() {
 //        frameRate(4f)
-
     }
 
     override fun draw() {
         background(255)
 
-//        if (mousePressed) {
-//            val vec = Vec(mouseX, mouseY)
-//            if (vec.squareDistance(lastPoint) > 5) {
-//                quadtree.insert(vec)
-//                lastPoint = vec
-//            }
+
+        // Building list of particles
+        val list = mouseTrace.update()
+//        val list = appWindow.listOfRandomVec(numParticles)
+
+        // Building tree
+        buildUp.start()
+
+        val quadtree = Quadtree(appWindow)
+        for (vec in list)
+            quadtree.insert(vec)
+
+        buildUp.end().println()
+
+        query.start()
+//        for (i in 1..numQueries) {
+//            val x = Random.nextFloat() * width
+//            val y = Random.nextFloat() * height
+//            val found = quadtree.query(Rect(x, y, 50f, 50f))
+////            found.drawAsCircles(g, 2f)
+////            println(found.size)
 //        }
-        val list = mutableListOf<Vec>()
-        for (i in 1..1000000) {
-            val x = Random.nextFloat() * width
-            val y = Random.nextFloat() * height
-            val vec = Vec(x, y)
-            list.add(vec)
-        }
-        val time = Timing()
-//        val quadtree = IndexQuadtree(list, rect, 40)
 
-            val quadtree = Quadtree(rect, 20)
-            for (vec in list)
-                quadtree.insert(vec)
+        val found = quadtree.query(Rect(mouseX, mouseY, 50, 50))
 
-//        fill(COLOR_RED.setAlpha(40))
-//        noStroke()
-//        quadtree.drawPoints(g, 8f)
-//
-//        noFill()
-//        stroke(COLOR_BLUE)
-//        strokeWeight(1f)
-//        quadtree.draw(g)
+        query.end().println()
 
-//        stroke(COLOR_GREEN)
-//        strokeWeight(1f)
-//        quadtree.drawIntersection(g, Rect(mouseX, mouseY, 10, 10))
 
-//        quadtree.drawPoints(g, 64f)
+        fillOnly(COLOR_RED.setAlpha(60))
+        list.drawAsCircles(g, 6f)
+        strokeOnly(COLOR_BLUE, 1f)
+        quadtree.draw(g)
 
-        println(time.get())
-
-        for (i in 1..10000) {
-            val x = Random.nextFloat() * width
-            val y = Random.nextFloat() * height
-            val found = quadtree.query(Rect(x, y, 50f, 50f))
-//            println(found.size)
-        }
-
-//        noStroke()
-//        fill(COLOR_FUCHSIA)
-//        list.drawAsCircles(g, 10f)
-        sum += time.get()
-        println(time.get())
-        println("Durchschnitt: " + (sum / frameCount.toFloat()))
+        fillOnly(COLOR_RED)
+        found.drawAsCircles(g, 6f)
     }
-
 }
 
 fun main(args: Array<String>) {
     MyApplet.runSketch()
 }
+
