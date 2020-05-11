5169e05623318416ba25781da69eb8fc874fbdff
Ulrich <songs.of.letting.go@gmail.com>
2020-05-11 13:04:02 +0200

    pointToPixels

--- midpoint-algo/src/main.kt
+++ midpoint-algo/src/main.kt
@@ -11,7 +11,7 @@ class MyApplet : PApplet() {
         }
     }
 
-    val resolution = 1200
+    val resolution = 1000
     val points = mutableListOf<Vec>()
     var grid = List(resolution.square()) { mutableListOf<Vec>() }
     val startRect by lazy { Rect(0, 0, width, height) }
@@ -24,174 +24,174 @@ class MyApplet : PApplet() {
     }
 
     override fun setup() {
-        image.loadPixels()
-        for (y in 0 until height)
-            for (x in 0 until width) {
-                val i = x + y * width
-//                println(image.pixels[i].getBrightness())
-                val brightness = image.pixels[i].getBrightness()
-                if (brightness > 150) {
-                    val newPoint = Vec(x, y)
-                    points.add(newPoint)
-                    addToGrid(newPoint, resolution)
-
-                }
-//                    image.pixels[i] = COLOR_BLACK
-//                else
-//                    image.pixels[i] = COLOR_WHITE
-
-            }
-        image.updatePixels()
+//        image.loadPixels()
+//        for (y in 0 until height)
+//            for (x in 0 until width) {
+//                val i = x + y * width
+////                println(image.pixels[i].getBrightness())
+//                val brightness = image.pixels[i].getBrightness()
+//                if (brightness > 150) {
+//                    val newPoint = Vec(x, y)
+//                    points.add(newPoint)
+//                    addToGrid(newPoint, resolution)
+//
+//                }
+////                    image.pixels[i] = COLOR_BLACK
+////                else
+////                    image.pixels[i] = COLOR_WHITE
+//
+//            }
+//        image.updatePixels()
     }
 
     override fun draw() {
         background(0)
 //        image(image, 0f, 0f)
-        var newPoint = Vec(mouseX, mouseY)
-        if (mousePressed) {
-            if (points.isEmpty()) {
-                points.add(newPoint)
-                addToGrid(newPoint, resolution)
-            } else {
-                val lastPoint = points.last()
-                if (lastPoint.squareDistance(newPoint) > 2) {
-                    for (i in 1..4) {
-                        points.add(newPoint)
-                        addToGrid(newPoint, resolution)
-                        newPoint = newPoint + (Vec.fromRandomAngle() * 2)
-                    }
-                }
-            }
-        }
-        g.noStroke()
-        if (points.isEmpty()) {
-            return
-        }
-        noStroke()
-        fill(COLOR_AQUA.setAlpha(20))
-        var subGridsIndices = createSubGrids(10, 3)
-//        for (subGridIndices in subGridsIndices) {
-//            val quad = createQuad(grid, subGridIndices)
-//            if (quad != null) {
-//                quad.drawAsShape(g)
+//        var newPoint = Vec(mouseX, mouseY)
+//        if (mousePressed) {
+//            if (points.isEmpty()) {
+//                points.add(newPoint)
+//                addToGrid(newPoint, resolution)
+//            } else {
+//                val lastPoint = points.last()
+//                if (lastPoint.squareDistance(newPoint) > 2) {
+//                    for (i in 1..4) {
+//                        points.add(newPoint)
+//                        addToGrid(newPoint, resolution)
+//                        newPoint = newPoint + (Vec.fromRandomAngle() * 2)
+//                    }
+//                }
 //            }
 //        }
-//        subGridsIndices = createSubGrids(resolution, 2)
-//        for (subGridIndices in subGridsIndices) {
-//            val quad = createQuad(grid, subGridIndices)
-//            if (quad != null) {
-//                quad.drawAsCurvedShape(g)
-//            }
+//        g.noStroke()
+//        if (points.isEmpty()) {
+//            return
 //        }
-        fill(COLOR_GREEN.setAlpha(255))
-        subGridsIndices = createSubGrids(resolution, 5)
-        for (subGridIndices in subGridsIndices) {
-            val quad = createQuad(grid, subGridIndices)
-            if (quad != null) {
-                val index = ((quad[0].x + quad[0].y * width).toInt()).coerceIn(0, image.pixels.size -1)
-                fill(image.pixels[index].setAlpha(20))
-
-                quad.drawAsCurvedShape(g)
-            }
-        }
-        fill(COLOR_GREEN.setAlpha(255))
-        subGridsIndices = createSubGrids(resolution, 6)
-        for (subGridIndices in subGridsIndices) {
-            val quad = createQuad(grid, subGridIndices)
-            if (quad != null) {
-                val index = ((quad[0].x + quad[0].y * width).toInt()).coerceIn(0, image.pixels.size -1)
-                fill(image.pixels[index].setAlpha(120))
-
-                quad.drawAsCurvedShape(g)
-            }
-        }
-        fill(COLOR_GREEN.setAlpha(255))
-        subGridsIndices = createSubGrids(resolution, 5)
-        for (subGridIndices in subGridsIndices) {
-            val quad = createQuad(grid, subGridIndices)
-            if (quad != null) {
-                val index = ((quad[0].x + quad[0].y * width).toInt()).coerceIn(0, image.pixels.size -1)
-                fill(image.pixels[index].setAlpha(180).setBlue(255))
-
-                quad.drawAsCurvedShape(g)
-            }
-        }
-        fill(COLOR_GREEN.setAlpha(255))
-        subGridsIndices = createSubGrids(resolution, 4)
-        for (subGridIndices in subGridsIndices) {
-            val quad = createQuad(grid, subGridIndices)
-            if (quad != null) {
-                val index = ((quad[0].x + quad[0].y * width).toInt()).coerceIn(0, image.pixels.size -1)
-                fill(image.pixels[index].setAlpha(80))
-
-                quad.drawAsCurvedShape(g)
-            }
-        }
-//        fill(COLOR_BLUE.setAlpha(130))
-//        subGridsIndices = createSubGrids(resolution, 1)
-//        for (subGridIndices in subGridsIndices) {
-//            val quad = createQuad(grid, subGridIndices)
-//            if (quad != null) {
-//                quad.drawAsCurvedShape(g)
-//            }
-//        }
-//        fill(COLOR_RED.setAlpha(130))
-//        subGridsIndices = createSubGrids(resolution, 1)
-//        for (subGridIndices in subGridsIndices) {
-//            val quad = createQuad(grid, subGridIndices)
-//            if (quad != null) {
+//        noStroke()
+//        fill(COLOR_AQUA.setAlpha(20))
+//        var subGridsIndices = createSubGrids(10, 3)
+////        for (subGridIndices in subGridsIndices) {
+////            val quad = createQuad(grid, subGridIndices)
+////            if (quad != null) {
+////                quad.drawAsShape(g)
+////            }
+////        }
+////        subGridsIndices = createSubGrids(resolution, 2)
+////        for (subGridIndices in subGridsIndices) {
+////            val quad = createQuad(grid, subGridIndices)
+////            if (quad != null) {
 ////                quad.drawAsCurvedShape(g)
-//            }
-//        }
-//        subGridsIndices = createSubGrids(10, 5)
+////            }
+////        }
+//        fill(COLOR_GREEN.setAlpha(255))
+//        subGridsIndices = createSubGrids(resolution, 5)
 //        for (subGridIndices in subGridsIndices) {
 //            val quad = createQuad(grid, subGridIndices)
 //            if (quad != null) {
-//                quad.drawAsShape(g)
+//                val index = ((quad[0].x + quad[0].y * width).toInt()).coerceIn(0, image.pixels.size -1)
+//                fill(image.pixels[index].setAlpha(20))
+//
+//                quad.drawAsCurvedShape(g)
 //            }
 //        }
-
-//        fill(COLOR_WHITE)
-//        points.drawAsCircles(g, 16f)
-//        val midPoint = points.midPointAll()
+//        fill(COLOR_GREEN.setAlpha(255))
+//        subGridsIndices = createSubGrids(resolution, 6)
+//        for (subGridIndices in subGridsIndices) {
+//            val quad = createQuad(grid, subGridIndices)
+//            if (quad != null) {
+//                val index = ((quad[0].x + quad[0].y * width).toInt()).coerceIn(0, image.pixels.size -1)
+//                fill(image.pixels[index].setAlpha(120))
 //
-//        val rects = startRect.splitTo4(midPoint)
-//        var rectPoints = points.splitByPoint(midPoint)
-//        for (i in rects.indices) {
-//            fill(colors[i].setAlpha(90))
-//            rects[i].draw(g)
-//            fill(colors[i].setAlpha(220))
-//            rectPoints[i].drawAsCircles(g, 8f)
+//                quad.drawAsCurvedShape(g)
+//            }
 //        }
-//        rectPoints = rectPoints.filter { it.size != 0 }
-//        val midPoints = List<Vec>(rectPoints.size) { i -> rectPoints[i].midPointAll() }
-//        fill(COLOR_WHITE)
-////        midPoints.drawAsCircles(g)
-//        if (midPoints.size >= 3) {
-//            fill(COLOR_FUCHSIA.setAlpha(120))
-//            midPoints.drawAsCurvedShape(g)
+//        fill(COLOR_GREEN.setAlpha(255))
+//        subGridsIndices = createSubGrids(resolution, 5)
+//        for (subGridIndices in subGridsIndices) {
+//            val quad = createQuad(grid, subGridIndices)
+//            if (quad != null) {
+//                val index = ((quad[0].x + quad[0].y * width).toInt()).coerceIn(0, image.pixels.size -1)
+//                fill(image.pixels[index].setAlpha(180).setBlue(255))
+//
+//                quad.drawAsCurvedShape(g)
+//            }
 //        }
+//        fill(COLOR_GREEN.setAlpha(255))
+//        subGridsIndices = createSubGrids(resolution, 4)
+//        for (subGridIndices in subGridsIndices) {
+//            val quad = createQuad(grid, subGridIndices)
+//            if (quad != null) {
+//                val index = ((quad[0].x + quad[0].y * width).toInt()).coerceIn(0, image.pixels.size -1)
+//                fill(image.pixels[index].setAlpha(80))
 //
-//        val index = frameCount % 25
-//        grid[index].drawAsCircles(g, 32f)
+//                quad.drawAsCurvedShape(g)
+//            }
+//        }
+////        fill(COLOR_BLUE.setAlpha(130))
+////        subGridsIndices = createSubGrids(resolution, 1)
+////        for (subGridIndices in subGridsIndices) {
+////            val quad = createQuad(grid, subGridIndices)
+////            if (quad != null) {
+////                quad.drawAsCurvedShape(g)
+////            }
+////        }
+////        fill(COLOR_RED.setAlpha(130))
+////        subGridsIndices = createSubGrids(resolution, 1)
+////        for (subGridIndices in subGridsIndices) {
+////            val quad = createQuad(grid, subGridIndices)
+////            if (quad != null) {
+//////                quad.drawAsCurvedShape(g)
+////            }
+////        }
+////        subGridsIndices = createSubGrids(10, 5)
+////        for (subGridIndices in subGridsIndices) {
+////            val quad = createQuad(grid, subGridIndices)
+////            if (quad != null) {
+////                quad.drawAsShape(g)
+////            }
+////        }
 //
-//        val subGrids = createSubGrids(5, 4)
-//        for (sub in subGrids)
-//            println(grid.sumSize())
-//        println()
+////        fill(COLOR_WHITE)
+////        points.drawAsCircles(g, 16f)
+////        val midPoint = points.midPointAll()
+////
+////        val rects = startRect.splitTo4(midPoint)
+////        var rectPoints = points.splitByPoint(midPoint)
+////        for (i in rects.indices) {
+////            fill(colors[i].setAlpha(90))
+////            rects[i].draw(g)
+////            fill(colors[i].setAlpha(220))
+////            rectPoints[i].drawAsCircles(g, 8f)
+////        }
+////        rectPoints = rectPoints.filter { it.size != 0 }
+////        val midPoints = List<Vec>(rectPoints.size) { i -> rectPoints[i].midPointAll() }
+////        fill(COLOR_WHITE)
+//////        midPoints.drawAsCircles(g)
+////        if (midPoints.size >= 3) {
+////            fill(COLOR_FUCHSIA.setAlpha(120))
+////            midPoints.drawAsCurvedShape(g)
+////        }
+////
+////        val index = frameCount % 25
+////        grid[index].drawAsCircles(g, 32f)
+////
+////        val subGrids = createSubGrids(5, 4)
+////        for (sub in subGrids)
+////            println(grid.sumSize())
+////        println()
+////
 //
-
-//        fill(COLOR_BLUE.setAlpha(60))
-//        points.drawAsCircles(g)
-//        val rect1 = Rect(0f, 0f, midPoint.x, midPoint.y)
-//        val pointsInRect1 = points.filter { rect1.contains(it) }
-//        fill(COLOR_FUCHSIA)
-//        pointsInRect1.drawAsCircles(g)
-//        fill(COLOR_FUCHSIA.setAlpha(50))
-//        rect1.draw(g)
-//        val m1 = pointsInRect1.midPoint()
-//        fill(COLOR_GREEN)
-//        m1.drawAsCircle(g)
+////        fill(COLOR_BLUE.setAlpha(60))
+////        points.drawAsCircles(g)
+////        val rect1 = Rect(0f, 0f, midPoint.x, midPoint.y)
+////        val pointsInRect1 = points.filter { rect1.contains(it) }
+////        fill(COLOR_FUCHSIA)
+////        pointsInRect1.drawAsCircles(g)
+////        fill(COLOR_FUCHSIA.setAlpha(50))
+////        rect1.draw(g)
+////        val m1 = pointsInRect1.midPoint()
+////        fill(COLOR_GREEN)
+////        m1.drawAsCircle(g)
     }
 
     fun addToGrid(vec: Vec, resolution: Int = 10) {
